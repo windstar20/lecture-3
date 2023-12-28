9773c44667cc1bd335715604055121e5b57473be
badagogi <badagogi@gmail.com>
2023-12-28 22:45:28 +0900

    image loading

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 function Card(props) {
-	return (
-		<div className="Card text-center">
-			<img src={props.image}/>
-			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
-				{props.children}
-			</div>
-		</div>
-	)
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const callback = (entries, observer) => {
+      console.log("callback ", entries, observer);
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("is Intersection", entry.target.dataset.src);
+          entry.target.previousSibling.srcset =
+            entry.target.previousSibling.dataset.srcset;
+          entry.target.src = entry.target.dataset.src;
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+    const options = {};
+
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(imgRef.current);
+  }, []);
+
+  return (
+    <div className="Card text-center">
+      <picture>
+        <source data-srcset={props.webp} type="image/webp" />
+        <img ref={imgRef} data-src={props.image} />
+      </picture>
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
